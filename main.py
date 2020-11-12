c4dc869b5c8a0fd4b6303b143c3cd2a30ab1fc8a
JarcaAndrei <69394169+JarcaAndrei@users.noreply.github.com>
2020-11-12 13:48:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -508,7 +508,7 @@ class App(Tk):
                         while loop:
                             # the loop will check if there are only 5 participants left and it'll exit and at the same time if another meeting has started
                             # if it exits before the time the next meeting has started, ok becomes 1 , loop 0 and we go back to the main if and will start from there again
-                            numOfParticipants=int(driver.find_element_by_xpath('/html/body/div[1]/c-wiz/div[1]/div/div[6]/div[3]/div[6]/div[3]/div/div[2]/div[1]/span/span/div/div/span[2]').get_attribute('innerHTML'))
+                            numOfParticipants=int(driver.find_element_by_xpath('/html/body/div[1]/c-wiz/div[1]/div/div[7]/div[3]/div[6]/div[3]/div/div[2]/div[1]/span/span/div/div/span[2]').get_attribute('innerHTML'))
                             if numOfParticipants<=5:
                                 c=1
                             if c==1:
